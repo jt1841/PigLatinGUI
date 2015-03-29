3fc4819a4ebfe7c6ad40749ecc2a1550ef692fd9
Jonathan Talbott <jt1841@gmail.com>
2015-03-29 04:43:58 -0400

    Final Release

---
+++ PigLatin.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace PigLatinGUI
+{
+    partial class PigLatin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.englishInput = new System.Windows.Forms.TextBox();
+            this.pigLatinOutput = new System.Windows.Forms.TextBox();
+            this.convertButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // englishInput
+            // 
+            this.englishInput.Location = new System.Drawing.Point(181, 52);
+            this.englishInput.Name = "englishInput";
+            this.englishInput.Size = new System.Drawing.Size(565, 20);
+            this.englishInput.TabIndex = 0;
+            this.englishInput.KeyDown += new System.Windows.Forms.KeyEventHandler(this.englishInput_KeyDown);
+            // 
+            // pigLatinOutput
+            // 
+            this.pigLatinOutput.Location = new System.Drawing.Point(181, 127);
+            this.pigLatinOutput.Name = "pigLatinOutput";
+            this.pigLatinOutput.Size = new System.Drawing.Size(565, 20);
+            this.pigLatinOutput.TabIndex = 1;
+            // 
+            // convertButton
+            // 
+            this.convertButton.Location = new System.Drawing.Point(181, 90);
+            this.convertButton.Name = "convertButton";
+            this.convertButton.Size = new System.Drawing.Size(75, 23);
+            this.convertButton.TabIndex = 2;
+            this.convertButton.Text = "Convert>>";
+            this.convertButton.UseVisualStyleBackColor = true;
+            this.convertButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(96, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "English";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(96, 127);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Pig Latin";
+            // 
+            // PigLatin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(838, 194);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.convertButton);
+            this.Controls.Add(this.pigLatinOutput);
+            this.Controls.Add(this.englishInput);
+            this.Name = "PigLatin";
+            this.Text = "Pig Latin Converter";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox englishInput;
+        private System.Windows.Forms.TextBox pigLatinOutput;
+        private System.Windows.Forms.Button convertButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+
+
+
+    }
+}
+
---
+++ PigLatin.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PigLatinGUI
+{
+    public partial class PigLatin : Form
+    {
+        public PigLatin()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            pigLatinOutput.Text = convertToPigLatin(englishInput.Text);
+        }
+
+        private string convertToPigLatin(string input)
+        {
+
+            bool goodInput = true;
+            StringBuilder output = new StringBuilder();
+            string outputString = "";
+
+                string[] inputWords;
+
+                    inputWords = input.Split(' ');
+
+                    foreach (var word in inputWords)
+                    {
+                        if (!Regex.IsMatch(word, @"^[\p{L}]+$"))
+                        {
+                            outputString = "Please enter a phrase with latin characters only";
+                            goodInput = false;
+                            break;
+                        }
+                        else
+                        {
+                            goodInput = true;
+                        }
+                    }
+
+
+                if (goodInput)
+                {
+                    for (int i = 0; i < inputWords.Length; i++)
+                    {
+                        if (inputWords[i].Length <= 2)
+                        {
+                            output.Append(inputWords[i] + " ");
+                        }
+                        else if ("aeiouAEIOU,.'".IndexOf(inputWords[i][0]) >= 0)
+                        {
+                            output.Append(inputWords[i] + "way ");
+                        }
+                        else
+                        {
+                            if (Char.IsUpper(inputWords[i][0]))
+                            {
+                                output.Append(Char.ToUpper(inputWords[i][1]));
+                            }
+                            else
+                            {
+                                output.Append(inputWords[i][1]);
+                            }
+
+                            output.Append(inputWords[i].Substring(2, inputWords[i].Length - 2))
+                                  .Append(Char.ToLower(inputWords[i][0]))
+                                  .Append("ay ");
+                        }
+
+                    }
+                    
+                    outputString = output.ToString();
+
+                }
+                return outputString;
+            }
+
+        private void englishInput_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                button1_Click((object)sender, (EventArgs)e);
+            }
+        }
+        }
+    }
